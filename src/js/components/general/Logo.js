756d8792d968084a6afeda6e7c4bfffc7eed59f8
Brian Ellis <brian@briansresume.com>
2021-08-23 19:36:09 -0400

    beginning of scaffolding https://www.bottleonthetable.com/

---
+++ src/js/components/general/Logo.js
@@ -0,0 +1,10 @@
+function Logo() {
+  return (
+    <div className="site_logo">
+      <p className="top">BOT</p>
+      <p className="bottom">Bottle on the table</p>
+    </div>
+  );
+}
+
+export default Logo;
